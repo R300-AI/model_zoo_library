11b22afca8f8c1bbf59fe260e04e633de5b13450
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-08-14 16:56:18 +0800

    Update profiler.py

--- profiler.py
+++ profiler.py
@@ -1,9 +1,9 @@
 from torch.profiler import profile, record_function, ProfilerActivity
 import onnx_tool, argparse
 
-def TorchScript_Profiler(benchmark, inputs):
+def TorchScript_Profiler(model_path, inputs):
     # load model in TorchScript format.
-    model = torch.jit.load(f"./{benchmark}/origin.pt")
+    model = torch.jit.load(model_path)
     model.eval()
 
     with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True, with_flops=True) as prof:
@@ -12,22 +12,22 @@ def TorchScript_Profiler(benchmark, inputs):
           model(inputs)
     print(prof.key_averages().table(sort_by="cpu_time_total"))
 
-def ONNX_Profiler(benchmark, inputs):
-  m = onnx_tool.Model(f"./{benchmark}/origin.onnx", {'constant_folding': True, 'verbose': True, 'if_fixed_branch': 'else', 'fixed_topk': 0})
+def ONNX_Profiler(model_path, inputs):
+  m = onnx_tool.Model(model_path, {'constant_folding': True, 'verbose': True, 'if_fixed_branch': 'else', 'fixed_topk': 0})
   m.graph.graph_reorder_nodes()
   m.graph.shape_infer({'data': inputs.numpy().shape})
   m.graph.profile()
   print(m.graph.print_node_map())
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--model", default=None, type=str, help="The name of the model in ./models folder")
-parser.add_argument("-s", "--shape", default=None, type=list, help="The shape of inputs")
+parser.add_argument("-m", "--model_path", default=None, type=str, help="The name of the model in ./models folder")
+parser.add_argument("-i", "--input_size", default=None, type=list, help="The shape of inputs")
 args = parser.parse_args()
+
 if __name__ == '__main__':
-  benchmark = args.model
-  if benchmark.endwith('.pt'):
-    TorchScript_Profiler(benchmark, inputs)
-  elif benchmark.endwith('.onnx'):
-    TorchScript_Profiler(benchmark, inputs)
+  if args.model_path.endwith('.pt'):
+    TorchScript_Profiler(args.model_path, args.input_size)
+  elif args.model_path.endwith('.onnx'):
+    TorchScript_Profiler(args.model_path, args.input_size)
   else:
     raise "Benchmark Format are not support yet."

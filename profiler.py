f07808cf6b4afab22a53e485d72aa8fef718b64d
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-08-14 09:28:51 +0000

    Update profiler.py

--- profiler.py
+++ profiler.py
@@ -1,7 +1,6 @@
 from torch.profiler import profile, record_function, ProfilerActivity
-import torch, onnx_tool, platform, argparse
-
-
+import onnx_tool, argparse, torch
+import numpy as np
 
 def TorchScript_Profiler(model_path, inputs):
     # load model in TorchScript format.
@@ -9,7 +8,7 @@ def TorchScript_Profiler(model_path, inputs):
     model.eval()
 
     with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True, with_flops=True) as prof:
-      with record_function(benchmark):
+      with record_function(""):
         with torch.inference_mode():
           model(inputs)
     print(prof.key_averages().table(sort_by="cpu_time_total"))
@@ -23,15 +22,17 @@ def ONNX_Profiler(model_path, inputs):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--model_path", default=None, type=str, help="The name of the model in ./models folder")
-parser.add_argument("-i", "--input_size", default=None, type=list, help="The shape of inputs")
+parser.add_argument("-b", "--batch_size", default=None, type=int, help="Batch size when running")
+parser.add_argument("-i", "--input_size", default=None, type=str, help="The String of input size")
+
 args = parser.parse_args()
 
 if __name__ == '__main__':
-  print(platform.system(), platform.processor(), platform.platform(), platform.machine(), platform.version(), platform.uname())
-    
-  if args.model_path.endwith('.pt'):
-    TorchScript_Profiler(args.model_path, args.input_size)
-  elif args.model_path.endwith('.onnx'):
-    TorchScript_Profiler(args.model_path, args.input_size)
+  inputs = torch.from_numpy(np.zeros(np.array(args.input_size.split(',')).astype(int))).float()
+  
+  if args.model_path.endswith('.pt'):
+    TorchScript_Profiler(args.model_path, inputs)
+  elif args.model_path.endswith('.onnx'):
+    ONNX_Profiler (args.model_path, inputs)
   else:
     raise "Benchmark Format are not support yet."

a1e77b562dfc8ad84b03d9335d29275f42e3cfdf
MarkovChenITRI <markov.chen1996@itri.org.tw>
2024-08-16 18:37:23 +0800

    update

--- install_armnn.sh
+++ install_armnn.sh
@@ -1,2 +1,2 @@
-sudo ln $LD_LIBRARY_PATH/libarmnnDelegate.so.29.0 libarmnnDelegate.so.29
-sudo ln $LD_LIBRARY_PATH/llibarmnn.so.33 llibarmnn.so.33
+sudo ln ${LD_LIBRARY_PATH}/libarmnnDelegate.so.29.0 libarmnnDelegate.so.29
+sudo ln ${LD_LIBRARY_PATH}/llibarmnn.so.33 llibarmnn.so.33

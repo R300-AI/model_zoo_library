01133a8e6e66d63ebdfc26f648c1336b7d43d674
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-08-16 09:39:39 +0000

    Create install_armnn.sh

---
+++ install_armnn.sh
@@ -0,0 +1,6 @@
+wget -O ArmNN-aarch64.tgz https://github.com/ARM-software/armnn/releases/download/v23.08/ArmNN-linux-aarch64.tar.gz
+mkdir armnn
+tar -xvf ArmNN-aarch64.tgz -C armnn
+
+sudo ln ../armnn/libarmnnDelegate.so.29.0 libarmnnDelegate.so.29
+sudo ln ../armnn/libarmnn.so.33.0 libarmnn.so.33

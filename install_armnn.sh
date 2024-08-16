2280bb1bc6d8bc075dc798b454d714eb94806770
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-08-16 10:17:19 +0000

    Update install_armnn.sh

--- install_armnn.sh
+++ install_armnn.sh
@@ -1,6 +1,1 @@
-wget -O ArmNN-aarch64.tgz https://github.com/ARM-software/armnn/releases/download/v23.08/ArmNN-linux-aarch64.tar.gz
-mkdir armnn
-tar -xvf ArmNN-aarch64.tgz -C armnn
-
-sudo ln ./armnn/libarmnnDelegate.so.29.0 libarmnnDelegate.so.29
-sudo ln ./armnn/libarmnn.so.33.0 libarmnn.so.33
+sudo ln /home/uibuntu/armnn/libarmnnDelegate.so.29.0 libarmnnDelegate.so.29

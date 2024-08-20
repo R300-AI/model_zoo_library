c97e73c132ee60f8e5e417f0543f27a451093a79
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-08-20 17:36:58 +0800

    Update benchmark.py

--- libs/benchmark.py
+++ libs/benchmark.py
@@ -19,3 +19,25 @@ def delegate(chipset):
   elif model_path.endswith('.tflite'):
     from profiler import TFLite_Profiler
     return TFLite_Profiler(model_path, chipset)
+
+class Genio_Benchmark(chipset, model_path, input_size=None):
+  def __init__():
+    ext = model_path.split('.')[-1]
+    engine = {'cpu': ['.pt', '.onnx', '.tflite'], 'gpu': ['.pt'], 'apu': ['.onnx']}
+    assert ext in engine[chipset], f"{ext} format are not support for {chipset}."
+
+    self.profiler = [delegate(chipset, model_path) for format in engine[chipset] if format==ext]
+    self.profiler.run(input_size)
+
+def delegate(chipset):
+  if model_path.endswith('.pt'):
+    from profiler import TorchScript_Pofiler
+    return TorchScript_Pofiler(model_path, chipset)
+  
+  elif model_path.endswith('.onnx'):
+    from profiler import ONNX_Profiler
+    return ONNX_Profiler(model_path, chipset)
+  
+  elif model_path.endswith('.tflite'):
+    from profiler import TFLite_Profiler
+    return TFLite_Profiler(model_path, chipset)

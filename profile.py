14c96ee4ebfa5b6b856dc3007c9f73a332dccb08
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-08-20 17:36:43 +0800

    Update profile.py

--- profile.py
+++ profile.py
@@ -5,7 +5,7 @@ from libs.profiler import General_Benchmark, Genio_Benchmark
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--model", default=None, type=str, required=True, help="The path to the candidate model. (e.g. ./../*.onnx, ./../*.tflite)")
 parser.add_argument("-p", "--processor", default='general', type=str, choices=['genio'] , help=".")
-parser.add_argument("-c", "--chipset", default=None, type=str, required=True,choices=['CPU', 'GPU', 'APU'] , help=".")
+parser.add_argument("-c", "--chipset", default='cpu', type=str, choices=['cpu', 'gpu', 'apu'] , help=".")
 parser.add_argument("-i", "--input_size", default=None, type=str, required=True , help="the shape of inputs. (e.g. '1, 3, 244, 244')")
 args = parser.parse_args()
 

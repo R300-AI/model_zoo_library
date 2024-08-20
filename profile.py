b2074ac756f4e2f82a2044fb94ec0e6fae0d64a8
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-08-20 16:55:31 +0800

    Update profile.py

--- profile.py
+++ profile.py
@@ -4,7 +4,7 @@ from libs.profiler import CPU_Benchmarks, GPU_Benchmarks
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--model", default=None, type=str, required=True, help="The path to the candidate model. (e.g. ./../*.onnx, ./../*.tflite)")
-parser.add_argument("-p", "--processor", default=None, type=str, choices=['Genio'] , help=".")
+parser.add_argument("-p", "--processor", default='general', type=str, choices=['genio'] , help=".")
 parser.add_argument("-c", "--chipset", default=None, type=str, required=True,choices=['CPU', 'GPU'] , help=".")
 parser.add_argument("-i", "--input_size", default=None, type=str, required=True , help="the shape of inputs. (e.g. '1, 3, 244, 244')")
 args = parser.parse_args()

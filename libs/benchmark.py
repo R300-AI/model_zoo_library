deba7b3237a6c5bbdecdf3b6a2b913d59d1d80be
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-08-20 17:45:48 +0800

    Update benchmark.py

--- libs/benchmark.py
+++ libs/benchmark.py
@@ -2,9 +2,6 @@ class General_Benchmark():
   def __init__(self, chipset, model_path, input_size=None):
     ext = model_path.split('.')[-1]
     engine = {'cpu': ['pt', 'onnx', 'tflite'], 'gpu': ['pt'], 'apu': ['onnx']}
-    print(model_path)
-    print(ext)
-    print(engine[chipset])
     assert ext in engine[chipset], f"{ext} format are not support for {chipset}."
 
     self.profiler = [delegate(chipset, model_path) for format in engine[chipset] if format==ext]

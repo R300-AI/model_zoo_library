7a020bcbfe211cd481b18e488100ec1a8c180e3b
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-08-20 17:38:43 +0800

    Update benchmark.py

--- libs/benchmark.py
+++ libs/benchmark.py
@@ -7,19 +7,6 @@ class General_Benchmark():
     self.profiler = [delegate(chipset, model_path) for format in engine[chipset] if format==ext]
     self.profiler.run(input_size)
 
-def delegate(chipset):
-  if model_path.endswith('.pt'):
-    from profiler import TorchScript_Pofiler
-    return TorchScript_Pofiler(model_path, chipset)
-  
-  elif model_path.endswith('.onnx'):
-    from profiler import ONNX_Profiler
-    return ONNX_Profiler(model_path, chipset)
-  
-  elif model_path.endswith('.tflite'):
-    from profiler import TFLite_Profiler
-    return TFLite_Profiler(model_path, chipset)
-
 class Genio_Benchmark():
   def __init__(self, chipset, model_path, input_size=None):
     ext = model_path.split('.')[-1]
